3ead479778232c0bac7a73794923e3760daec496
Dennis Byberg <dennis.byberg@hotmail.com>
2022-04-03 02:32:27 +0200

    Stage and tetrominos

--- my-tetris-game/src/components/Tetris.jsx
+++ my-tetris-game/src/components/Tetris.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { createStage } from "../gameHelpers";
 // Components...
 import Stage from "./Stage";
 import Display from "./Display";
@@ -7,7 +8,7 @@ import StartButton from "./StartButton";
 const Tetris = () => {
   return (
     <div>
-      <Stage />
+      <Stage stage={createStage()} />
       <aside>
         <div>
           <Display text="Score" />
