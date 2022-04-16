2e30c19372bc1e73639e9151c38702f05d78dc9d
Dennis Byberg <dennis.byberg@hotmail.com>
2022-04-16 17:46:45 +0200

    skapat sidor med hjälp av routing

--- dennis-byberg-poke-manager/src/App.jsx
+++ dennis-byberg-poke-manager/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import SearchPage from "./pages/SearchPage";
+import TeamPage from "./pages/TeamPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/team" element={<TeamPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
