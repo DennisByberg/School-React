f9a363962d9de368b6a98dbb7844cdfc1123d531
Dennis Byberg <dennis.byberg@hotmail.com>
2022-04-21 18:05:43 +0200

    Funkar nu att lägga till pokemons från listan

--- dennis-byberg-poke-manager/src/pages/TeamPage.jsx
+++ dennis-byberg-poke-manager/src/pages/TeamPage.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "./TeamPage.module.css";
 
 const TeamPage = ({ team, setTeam }) => {
   return (
-    <div>
-      TeamPage
-      <div>{team}</div>
+    <div className={classes.teampage}>
+      <h1>Team Page</h1>
+      <ul>
+        {team.map((t) => (
+          <li key={t.name}>
+            <button>x</button>
+            <button>Change Name</button>
+            {t.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
